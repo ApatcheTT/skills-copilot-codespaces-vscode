e5e267680c8d146b37fb7695cdcfd33ffd2be2fd
ApatcheTT <165249311+ApatcheTT@users.noreply.github.com>
2024-06-10 10:25:39 +1000

    Update comments.js

--- comments.js
+++ comments.js
@@ -8,42 +8,6 @@
 // npm install passport passport-local passport-local-mongoose express-session
 // npm install connect-flash
 
-var express = require("express"),
-    app = express(),
-    bodyParser = require("body-parser"),
-    mongoose = require("mongoose"),
-    Comment = require("./models/comment"),
-    Campground = require("./models/campground"),
-    seedDB = require("./seeds"),
-    passport = require("passport"),
-    LocalStrategy = require("passport-local"),
-    User = require("./models/user"),
-    methodOverride = require("method-override"),
-    flash = require("connect-flash");
 
-// Requiring Routes
-var commentRoutes = require("./routes/comments"),
-    campgroundRoutes = require("./routes/campgrounds"),
-    indexRoutes = require("./routes/index");
-
-// seedDB();
-mongoose.connect("mongodb://localhost/yelp_camp_v10");
-app.use(bodyParser.urlencoded({extended: true}));
-app.set("view engine", "ejs");
-app.use(express.static(__dirname + "/public"));
-app.use(methodOverride("_method"));
-app.use(flash());
-
-// Passport Configuration
-app.use(require("express-session")({
-    secret: "Once again Rusty wins cutest dog!",
-    resave: false,
-    saveUninitialized: false
-}));
-app.use(passport.initialize());
-app.use(passport.session());
-passport.use(new LocalStrategy(User.authenticate()));
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
 
 // Middleware to pass user to all
